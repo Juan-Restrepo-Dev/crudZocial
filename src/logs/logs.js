4907e10e3603913bcd97a5ecc9095d30f5423666
Delvidavid <david.990320@gmail.com>
2025-07-02 23:50:51 -0500

    logs.js

---
+++ src/logs/logs.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const tables = document.getElementById("contenido")
+
+    const history = JSON.parse(localStorage.getItem("Action")) || [];
+
+    history.forEach((item,index)=>{
+        const fila = document.createElement("tr"); 
+
+        fila.innerHTML =`
+
+            <td>${index + 1}</td>
+            <td>${item.action}</td>
+            <td>${item.date}</td>
+        `;
+        
+
+        tables.appendChild(fila);
+    });
+
+});
